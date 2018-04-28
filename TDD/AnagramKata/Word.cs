f8075d769ea9940256bfe06d8d293b6d854be80f
Jimmy, Lee <phxswjjj@gmail.com>
2018-04-28 14:52:19 +0800

    Anagram Test Word

---
+++ TDD/AnagramKata/Word.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.AnagramKata
+{
+    class Word
+    {
+        public Word(string w)
+        {
+            this.Source = w;
+
+            //bbca to abbc
+            var sorted = w.OrderBy(c => c);
+            this.Key = string.Join("", sorted);
+        }
+        public string Source { get; private set; }
+        public string Key { get; private set; }
+    }
+}
---
+++ TDD/AnagramKata/WordTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.AnagramKata
+{
+    [TestClass]
+    public class WordTests
+    {
+        [DataTestMethod]
+        [DataRow("", "")]
+        [DataRow("bca", "abc")]
+        [DataRow("bcba", "abbc")]
+        [DataRow("aaa", "aaa")]
+        [DataRow("bBaAaa", "ABaaab")]
+        public void TestWord(string word, string exceptKey)
+        {
+            var w = new Word(word);
+            Assert.AreEqual(exceptKey, w.Key);
+        }
+    }
+}
